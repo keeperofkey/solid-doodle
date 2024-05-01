40f9a7b6cea5b637e293c8770464aadadc44ea09
keeperofkey <doddtastic@gmail.com>
2024-05-01 11:26:58 +0100

    fix(joya): overlay add text

--- src/routes/joya/overlay.svelte
+++ src/routes/joya/overlay.svelte
@@ -2,19 +2,26 @@
 </script>
 
 <div class="overlay">
-    <img src="images/cans-1.webp" alt="First image from artist residency" />
+    <img src="images/cans-1.webp" alt="from artist residency" />
     <div></div>
+    <div class="text">Images from an artist residency in Spain</div>
+    <div class="text">
+        Exploring the cravases cut into the landscape by monumental floods
+    </div>
     <div></div>
+    <img src="images/cans-2.webp" alt="from artist residency" />
+    <img src="images/cans-3.webp" alt="from artist residency" />
     <div></div>
+    <div class="text">
+        Castings made from varying mixtures of earth and cement using cans and
+        debris
+    </div>
+    <div class="text">Meant to erode</div>
     <div></div>
-    <img src="images/cans-2.webp" alt="Second image from artist residency" />
-    <img src="images/cans-3.webp" alt="Third image from artist residency" />
+    <img src="images/cans-5.webp" alt="from artist residency" />
+    <img src="images/cans-7.webp" alt="from artist residency" />
     <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <img src="images/cans-5.webp" alt="Fourth image from artist residency" />
-    <img src="images/cans-7.webp" alt="Fifth image from artist residency" />
+    <div class="text">left to disperse</div>
 </div>
 
 <style>
@@ -29,6 +36,11 @@
         border-radius: 0.5rem;
         box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
     }
-    
-
+    .text {
+        font-size: 1.5rem;
+        font-weight: 600;
+        text-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
+        text-align: center;
+        align-self: center;
+    }
 </style>
